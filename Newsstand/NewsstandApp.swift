4f6718e210b407613f5c833e11264006916fdf79
Bruno Castelló <bfcastello@gmail.com>
2024-08-03 16:35:25 -0300

    Initial Commit

---
+++ Newsstand/NewsstandApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsstandApp.swift
+//  Newsstand
+//
+//  Created by Bruno Castelló on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsstandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
